0a5c6fdb528c93c485387d1a98bf9bb8b5699ce1
Naveen G <41387473+AquamanRanda@users.noreply.github.com>
2020-04-29 16:19:13 +0530

    Updates

---
+++ precache-manifest.902dbf27c87ef10580412d1e8f7b8a34.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b36afe87ea0592c34745528c082af1a",
+    "url": "/DeepTea/index.html"
+  },
+  {
+    "revision": "0bf97f6b454cc774c371",
+    "url": "/DeepTea/static/css/main.713aa6bd.chunk.css"
+  },
+  {
+    "revision": "e76ee8389e6f660d03e0",
+    "url": "/DeepTea/static/js/2.5d238399.chunk.js"
+  },
+  {
+    "revision": "0bf97f6b454cc774c371",
+    "url": "/DeepTea/static/js/main.c10a29f2.chunk.js"
+  },
+  {
+    "revision": "7f21750bcbe0b43c2ba7",
+    "url": "/DeepTea/static/js/runtime~main.bcb3fd1f.js"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/DeepTea/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/DeepTea/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/DeepTea/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/DeepTea/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/DeepTea/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "df0cdbfb2c70d9857e3776eb83a5a4eb",
+    "url": "/DeepTea/static/media/creative-tim-white-slim2.df0cdbfb.png"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/DeepTea/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/DeepTea/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/DeepTea/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/DeepTea/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/DeepTea/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/DeepTea/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/DeepTea/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/DeepTea/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/DeepTea/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/DeepTea/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/DeepTea/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/DeepTea/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/DeepTea/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/DeepTea/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/DeepTea/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "7fb9995f149084df87e4c12fb6373138",
+    "url": "/DeepTea/static/media/services.7fb9995f.jpg"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/DeepTea/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/DeepTea/static/media/uriel-soberanes.fe3a7838.jpg"
+  }
+]);
